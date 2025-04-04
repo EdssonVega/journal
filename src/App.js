29950f339b6c1e5f225f69f8d3fc864b5f8f840f
Edsson <e_d_sx2@hotmail.com>
2025-04-04 18:35:56 -0400

    added global variable: primary color

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import { AppRouter } from './router/AppRouter';
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <>
+      <AppRouter />
+    </>
+
+    
   );
 }
 
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { AuthRouter } from '../auth/routes/AuthRouter'
+import { JournalRouter } from '../journal/routes/JournalRouter'
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+        <Route path='auth/*' element={<AuthRouter />}/>
+        <Route path='/*' element={<JournalRouter />}/>
+    </Routes>
+  )
+}
