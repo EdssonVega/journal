1c684d7fc31841e9cf590d8b95ecf5cad2866148
Edsson <e_d_sx2@hotmail.com>
2025-04-06 05:31:45 -0400

    starting login page

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { FaGoogle } from "react-icons/fa";
+import { NavLink } from 'react-router-dom';
 import "./loginPageStyles.css"
 
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className='bgContainer'>
+      <div className='formContainer'>
+        <h1>Login</h1>
+        <div className='labelAnimationContainer'>
+        <label htmlFor="">Email</label>
+        <input type="email" />
+
+        </div>
+        <div className='labelAnimationContainer'>
+        <label htmlFor="">Password</label>
+        <input type="password" />
+
+        </div>
+        <div>
+          <button>
+            LOGIN
+          </button>
+          <button>
+          <FaGoogle />
+            GOOGLE
+          </button>
+        </div>
+        <NavLink >Crear una cuenta</NavLink>
+      </div>
+    </div>
   )
 }
