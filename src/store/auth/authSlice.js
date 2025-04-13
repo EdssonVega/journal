18708309b632406fda5808a329debf5b8e5dcc80
Edsson <e_d_sx2@hotmail.com>
2025-04-13 01:43:48 -0400

    configured redux store

---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        status: "checking", // "not-authenticated","authenticated"  
+        uid:null,
+        email:null,
+        displayName:null,
+        errorMessage:null
+    },
+    reducers: {
+        login: (state, /* action */ ) => {
+
+        },
+        logout: (state, payload ) => {
+            
+        },
+        chekingCredentials: (state, /* action */ ) => {
+            
+        },
+    }
+});
+
+// Action creators are generated for each case reducer function
+export const {login, logout, chekingCredentials } = authSlice.actions;
