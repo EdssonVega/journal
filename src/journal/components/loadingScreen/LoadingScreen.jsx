0f733c968dc3653c0386225e319870cbea7b1aae
Edsson <e_d_sx2@hotmail.com>
2025-04-19 21:52:15 -0400

    added loading screen animation

---
+++ src/journal/components/loadingScreen/LoadingScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./loadingScreenStyles.css";
+
+export const LoadingScreen = () => {
+  return (
+    <div className="loadScreenContainer">
+      <div class="circleContainer">
+        <div class="circleFill"></div>
+      </div>
+    </div>
+  );
+};
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Route, Routes } from 'react-router-dom'
 import { AuthRouter } from '../auth/routes/AuthRouter'
+import { LoadingScreen } from '../journal/components/loadingScreen/LoadingScreen'
 import { JournalRouter } from '../journal/routes/JournalRouter'
 
 export const AppRouter = () => {
+
+  const {status} = useSelector(state => state.auth)
+
+  if(status === "checking"){
+    return (
+      <LoadingScreen />
+    )
+  }
+
   return (
     <Routes>
         <Route path='auth/*' element={<AuthRouter />}/>
